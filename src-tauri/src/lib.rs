c4ebd9dc113b4c3fc0079f8db47ad487ff08c276
Tom Barone <mail@tombarone.net>
2024-10-06 20:29:18 +0200

    Add stubbed transcript streaming

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,74 @@
+use crossbeam::atomic::AtomicCell;
+use std::sync::{Arc, Mutex};
+use tauri::{AppHandle, Emitter};
+
+pub type Thread = std::thread::JoinHandle<()>;
+
+/// The main application state is stored in this struct
+pub struct State {
+    /// The settings for the application
+    pub transcript: Arc<Mutex<String>>,
+    pub thread: Arc<Mutex<Option<Thread>>>,
+    pub stop_flag: Arc<AtomicCell<bool>>,
+}
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command]
+fn start_recording(app: AppHandle, state: tauri::State<State>) {
+    state.transcript.lock().unwrap().clear();
+    state.transcript.lock().unwrap().push_str("DJ Khaled: ");
+    state.stop_flag.store(false);
+    let stop_flag = state.stop_flag.clone();
+    let transcript_arc = state.transcript.clone();
+    let thread = std::thread::spawn(move || {
+        // Send a transcript event every second
+        loop {
+            if stop_flag.load() {
+                break;
+            }
+            std::thread::sleep(std::time::Duration::from_millis(500));
+            let mut new_transcript = transcript_arc.lock().unwrap().clone();
+            new_transcript.push_str("another one, ");
+            transcript_arc.lock().unwrap().clear();
+            transcript_arc.lock().unwrap().push_str(&new_transcript);
+            app.emit("transcript", new_transcript).unwrap();
+        }
+    });
+    state.thread.lock().unwrap().replace(thread);
+}
+
+#[tauri::command]
+fn stop_recording(state: tauri::State<State>) {
+    // Stop the thread
+    state.stop_flag.store(true);
+    let thread = state.thread.lock().unwrap().take();
+    if let Some(thread) = thread {
+        thread.join().unwrap();
+    }
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    // This state object will be shared across all our tauri controllers
+    let state = State {
+        transcript: Arc::new(Mutex::new("".to_string())),
+        thread: Arc::new(Mutex::new(None)),
+        stop_flag: Arc::new(AtomicCell::new(false)),
+    };
+
     tauri::Builder::default()
+        .manage(state)
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![
+            greet,
+            start_recording,
+            stop_recording,
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,71 +1,91 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+  import { onMount } from "svelte";
+  import { listen } from "@tauri-apps/api/event";
+  import { invoke } from "@tauri-apps/api/core";
 
-	let isRecording = false;
-	onMount(() => {
-		// Add keyboard event listeners
-		window.addEventListener('keydown', handleKeyDown);
-		window.addEventListener('keyup', handleKeyUp);
-	});
-	
+  let isRecording = false;
+  let transcript = "";
+  onMount(() => {
+    // Add keyboard event listeners
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("keyup", handleKeyUp);
+  });
 
-	function handleKeyDown(event: KeyboardEvent) {
-    if (event.code === 'Space' && !event.repeat && !isRecording) {
+  function handleKeyDown(event: KeyboardEvent) {
+    if (event.code === "Space" && !event.repeat && !isRecording) {
       event.preventDefault(); // Prevent page scrolling
       startRecording();
     }
   }
 
-	function handleKeyUp(event: KeyboardEvent) {
-    if (event.code === 'Space' && isRecording) {
+  function handleKeyUp(event: KeyboardEvent) {
+    if (event.code === "Space" && isRecording) {
       stopRecording();
     }
   }
 
-	function startRecording() {
-		isRecording = true;
-	}
-	function stopRecording() {
-		isRecording = false;
-	}
+  function startRecording() {
+    invoke("start_recording").then(() => {
+      isRecording = true;
+    });
+  }
+  function stopRecording() {
+    invoke("stop_recording").then(() => {
+      isRecording = false;
+    });
+  }
+
+  listen<string>("transcript", (event) => {
+    transcript = event.payload;
+  });
 </script>
 
 <div class="container">
-	<h1>Talk to me</h1>
-	<button
-		on:mousedown={startRecording}
-		on:mouseup={stopRecording}
-		on:mouseleave={stopRecording}
-		on:touchstart={startRecording}
-		on:touchend={stopRecording}
-		class:recording={isRecording}
-	>
-		{isRecording ? "Recording..." : "Hold to Record"}
-	</button>
+  <h1>Talk to me, boy</h1>
+  <div class="transcript">{transcript}</div>
+  <button
+    on:mousedown={startRecording}
+    on:mouseup={stopRecording}
+    on:mouseleave={stopRecording}
+    on:touchstart={startRecording}
+    on:touchend={stopRecording}
+    class:recording={isRecording}
+  >
+    {isRecording ? "Recording..." : "Hold to Record"}
+  </button>
 </div>
 
 <style>
-	.container {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: space-between;
-		height: 90vh;
-	}
-	button.recording {
-		background-color: #f44336;
-	}
-	button {
-		padding: 20px 20px;
-		width: 240px;
-		font-size: 1.5em;
-		user-select: none;
-	}
+  .container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+    height: 90vh;
+  }
+  .transcript {
+    font-size: 1.5em;
+    margin: 20px 0;
+    white-space: pre-wrap;
+    text-align: center;
+  }
+  button.recording {
+    background-color: #f44336;
+  }
+  button {
+    padding: 20px 20px;
+    width: 240px;
+    font-size: 1.5em;
+    user-select: none;
+    border: none;
+    border-radius: 5px;
+    cursor: pointer;
+  }
 
-	@media (prefers-color-scheme: dark) {
-		:root {
-			background-color: #333;
-			color: #fff;
-		}
-	}
+  @media (prefers-color-scheme: dark) {
+    :root {
+      background-color: #333;
+      color: #fff;
+    }
+  }
 </style>
