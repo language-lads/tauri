53d2bc1a1719837e5d6ddf919b0d4028f7208a66
Tom Barone <mail@tombarone.net>
2024-10-10 10:48:59 +0200

    Add request permissions to microphone plugin

--- tauri-plugin-microphone/android/src/main/java/ExamplePlugin.kt
+++ tauri-plugin-microphone/android/src/main/java/ExamplePlugin.kt
@@ -7,19 +7,32 @@ import app.tauri.annotation.TauriPlugin
 import app.tauri.plugin.JSObject
 import app.tauri.plugin.Plugin
 import app.tauri.plugin.Invoke
+import androidx.core.content.ContextCompat
+import android.Manifest
+import android.content.pm.PackageManager
+import androidx.core.app.ActivityCompat
 
 @InvokeArg
 class PingArgs {
-  var value: String? = null
+    var value: String? = null
 }
 
 @TauriPlugin
-class ExamplePlugin(private val activity: Activity): Plugin(activity) {
+class ExamplePlugin(private val activity: Activity) : Plugin(activity) {
     private val implementation = Example()
 
     @Command
     fun ping(invoke: Invoke) {
         val args = invoke.parseArgs(PingArgs::class.java)
+
+        if (ContextCompat.checkSelfPermission(activity, Manifest.permission.RECORD_AUDIO)
+            == PackageManager.PERMISSION_DENIED
+        ) {
+            ActivityCompat.requestPermissions(
+                activity,
+                arrayOf(Manifest.permission.RECORD_AUDIO), 1001
+            )
+        }
 
         val ret = JSObject()
         ret.put("value", implementation.pong(args.value ?: "default value :("))
